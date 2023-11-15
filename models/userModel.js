6c220a0a1bc90cf5476a21f6db76ade04bdcb328
steve_D <stephenCdoherty@gmail.com>
2023-11-15 10:30:33 -0600

    created user model

---
+++ models/userModel.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const { Schema } = mongoose;
+
+// USER SCHEMA
+const userSchema = new Schema({
+  name: { type: String, required: [true, "Please add your name."] },
+  email: {
+    type: String,
+    required: [true, "Please add your email."],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, "Please provide a valid email."],
+  },
+  photo: { type: String, default: "http://placehold.it/500x500.png" },
+  password: {
+    type: String,
+    required: [true, "Please provide a password."],
+    minlength: 8,
+  },
+  confirmPassword: {
+    type: String,
+    required: [true, "Please confirm your password."],
+  },
+});
+
+//USER MODEL
+const User = mongoose.model("User", userSchema);
+
+//EXPORT USER
+module.exports = User;
