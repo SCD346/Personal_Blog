7f4fcaca76bebe65988cac62866023b7bf2e4737
steve_D <stephenCdoherty@gmail.com>
2023-11-21 12:45:44 -0600

    login set up

--- views/login.jsx
+++ views/login.jsx
@@ -4,23 +4,7 @@ const Default = require("./layouts/default");
 function Login() {
   return (
     <Default>
-      <div className="form">
-        <form>
-          <div className="input-container">
-            <label>Username </label>
-            <input type="text" name="uname" required />
-            {renderErrorMessage("uname")}
-          </div>
-          <div className="input-container">
-            <label>Password </label>
-            <input type="password" name="pass" required />
-            {renderErrorMessage("pass")}
-          </div>
-          <div className="button-container">
-            <input type="submit" />
-          </div>
-        </form>
-      </div>
+      <h2>Log In</h2>
     </Default>
   );
 }
